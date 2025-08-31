78f9e02f316eb2242dbdd8bc88c2036adfbb4e70
kaungthantlinn-coding <kaungthantlinn307@gmail.com>
2025-08-31 17:48:21 +0630

    Restore IExportService.cs and ExportService.cs files with correct implementations

--- src/DisasterApp.Application/Services/Interfaces/IExportService.cs
+++ src/DisasterApp.Application/Services/Interfaces/IExportService.cs
@@ -1,24 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using DisasterApp.Application.DTOs;
 
-namespace DisasterApp.Application.Services
+namespace DisasterApp.Application.Services.Interfaces;
+
+public interface IExportService
 {
-<<<<<<< HEAD
-    public interface IExportService
-    {
-        Task<byte[]> ExportDisasterReportsToExcelAsync();
-        Task<byte[]> ExportDisasterReportsToPdfAsync();
-    }
-=======
     Task<byte[]> ExportToCsvAsync(IEnumerable<AuditLogDto> data, List<string>? fields = null);
     Task<byte[]> ExportToExcelAsync(IEnumerable<AuditLogDto> data, List<string>? fields = null);
     Task<byte[]> ExportToPdfAsync(IEnumerable<AuditLogDto> data, List<string>? fields = null);
-    List<string> GetAvailableFields();//
+    List<string> GetAvailableFields();
     bool ValidateFields(List<string> fields);
     string GetMimeType(string format);
     string GetFileExtension(string format);
->>>>>>> ktldev
 }
+//
